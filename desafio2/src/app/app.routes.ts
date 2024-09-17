828c34e903a1c1a7f00e49261e09f04baaf18260
Dnjoc <131299098+dnjoc@users.noreply.github.com>
2024-09-17 15:42:35 -0400

    Desafio 2 Ionic nivel 2

---
+++ desafio2/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'linterna',
+    loadComponent: () => import('./linterna/linterna.page').then( m => m.LinternaPage)
+  },
+];
